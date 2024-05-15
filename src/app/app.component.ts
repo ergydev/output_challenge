9141295b1e0466e82398aeaa7792d2a87c4a33a5
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2024-05-15 17:23:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateOnomatopiaComponent } from './create-onomatopia/create-onomatopia.component';  
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, CreateOnomatopiaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'output_challenge';
+
+  onomatopoeiaList: string[] = [
+    'Bam',
+    'Boom',
+    'Crash',
+    'Kaboom',
+    'Pow',
+    'Smash',
+    'Whack',
+  ];
+
+  onReiceveNewOnomatopia(newOnomatopia: string): void {
+    this.onomatopoeiaList.push(newOnomatopia);
+  };
 }
